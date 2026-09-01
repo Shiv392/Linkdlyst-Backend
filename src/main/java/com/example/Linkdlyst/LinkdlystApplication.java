12ade4e1e3309b8eab0f9ce39d5ae0b8e6f91e4f
Shiv Soni <sonishiv309@gmail.com>
2026-09-01 16:50:39 +0530

    created spring boot project

---
+++ src/main/java/com/example/Linkdlyst/LinkdlystApplication.java
@@ -0,0 +1,13 @@
+package com.example.Linkdlyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkdlystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkdlystApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Linkdlyst/LinkdlystApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Linkdlyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkdlystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
